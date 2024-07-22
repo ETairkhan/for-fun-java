ab294b10829611a26803512af99e31749c169bb9
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-22 17:15:11 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int numbers = 0;
-        while (true){
-            System.out.println("Give a number: ");
-            int number = Integer.parseInt(scanner.nextLine());
-            if (number == 0) {
-                break;
-            }
+        for ( int i = scanner.nextInt() ; i <= 100; i++  ){
+            System.out.println(i);
+        }
 
-            if (number > 0){
-                sum = sum+number;
-                numbers++;
-            }
-
-        }
-        if (sum >0){
-            System.out.println((double) sum/numbers);
-        }
-        else{
-            System.out.println("cannot calculate the average");
-        }
     }
 }
