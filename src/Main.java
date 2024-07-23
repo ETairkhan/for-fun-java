01e74292d6cad0939f6a571070f9b8b86fc68354
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-23 15:51:39 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        for ( int i = scanner.nextInt() ; i <= 100; i++  ){
-            System.out.println(i);
+        int result = 1;
+        int s = 1;
+        System.out.print("Give a number ");
+        for (int i = scanner.nextInt(); s <=i; s++){
+            result *=  s;
         }
-
+        System.out.println("Factorial " + result);
     }
 }
