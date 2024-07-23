99bb3c337d75f66ffdc1bf3d90aa02ad625473cd
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-23 16:14:25 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int result = 1;
-        int s = 1;
-        System.out.print("Give a number ");
-        for (int i = scanner.nextInt(); s <=i; s++){
-            result *=  s;
+        System.out.println("Give numbers:");
+        int result = 0;
+        int count = 0;
+        double average = 0;
+        int even = 0;
+        int odd =0;
+        while (true){
+            int number = scanner.nextInt();
+            if(number == -1 ){
+                System.out.println("Thx! Bye!");
+                break;
+
+            }
+            if (number % 2 ==0 ){
+                even ++;
+            }
+            if (number % 2 !=0 ){
+                odd ++;
+            }
+            count++;
+            result +=number;
+            average = (double) result/count;
         }
-        System.out.println("Factorial " + result);
+        System.out.println("Sum:" + result);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + average);
+        System.out.println("Even:" + even);
+        System.out.println("Odd: " + odd);
     }
 }
