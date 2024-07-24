61fab9bfc27562b662ce4823ee7558f374877c91
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-24 12:30:13 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        printRectangle(17,3);
+        printTriangle(4);
     }
-    public static void printRectangle(int width, int height) {
-        int i = 1;
-        while(height >=i){
-            int s =1;
-            while(width >= s){
-                System.out.print("*");
-                s++;
-            }
-            System.out.println("");
-            i++;
-        }
+    public static void printTriangle(int size) {
+       int s= 1;
+       while(size>=s){
+           int i =1;
+           while(s>=i){
+               System.out.print("*");
+               i++;
+           }
+           s++;
+           System.out.println("");
+       }
     }
 }
