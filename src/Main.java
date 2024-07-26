f8944c49a39b6547b1884cbcbec0573dae74e1a4
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-26 11:45:58 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,18 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        printTriangle(4);
-    }
-    public static void printTriangle(int height) {
-        int i, space, stars;
-        // outer loop to handle rows
-        for (i = 1; i <=height ; i++) {
-
-            // inner loop to print spaces.
-            for (space = 1; space <= height-i; space++) {
-                System.out.print(" ");
+        Scanner x = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        while(true) {
+            String names = x.nextLine();
+            if (names.equals(" ")){
+                break;
             }
-
-            // inner loop to print stars.
-            for (stars = 1; stars <= i*2 - 1; stars++) {
-                System.out.print("*");
-            }
-
-            // printing new line for each row
-            System.out.println();
+            list.add(names);
         }
-        for( i = 1;i <= 2;i++)
-        {
-            System.out.print(" ");
-            for(int j = height-3;j > 0;j--)
-            {
-                System.out.print(" ");
-            }
-            for(int k = 3;k > 0;k--)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }// end of lower part
+
+        System.out.println(list.get(2));
     }
 }
