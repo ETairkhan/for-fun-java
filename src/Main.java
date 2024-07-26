c4935989208531ce10c9550e6a50217ed48918af
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-26 14:19:50 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> wordList = new ArrayList<>();
         Scanner x = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        while(true) {
-            String names = x.nextLine();
-            if (names.equals(" ")){
+        while(true){
+            String names= x.nextLine();
+            if(names.equals(" ")){
                 break;
             }
-            list.add(names);
+            wordList.add(names);
         }
-
-        System.out.println(list.get(2));
+        System.out.println("In total: " + wordList.size());
     }
 }
