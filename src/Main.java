a00e58a7addfe98f912447ea1575f9aac301ee34
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-27 20:01:19 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,38 @@
-import javax.crypto.spec.PSource;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int index = -1;
         while(true){
-            String name = scanner.nextLine();
-            if (name.equals(" ")){
+            int number = scanner.nextInt();
+            index++;
+            numbers.add(number);
+            if( number == 9999 ){
                 break;
             }
+        }
+        System.out.println("Search for?");
 
-            names.add(name);
-            index++;
+       int smallest = numbers.get(0);
+       for(int i = 0; i < numbers.size(); i++){
+            int number = numbers.get(i);
+            if ( smallest > number){
+                smallest = number;
+            }
+       }
+
+        int find = smallest;
+
+        for (int i = 0; i < index; i++ ){
+            int number = numbers.get(i);
+            if ( number == find){
+                System.out.println(find + " is at index " + i);
+            }
+
         }
 
-        System.out.println(names.get(0));
-        System.out.println(names.get(index));
     }
 }
