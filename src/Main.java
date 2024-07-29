c841f8bac2fa286090b98adfffe29c1b1ebd2551
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-29 17:55:07 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,23 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 3;
-        numbers[2] = 5;
+        int[] numbers = new int[7];
+        numbers[0] = 42;
+        numbers[1] = 13;
+        numbers[2] = 12;
         numbers[3] = 7;
-        numbers[4] = 9;
-        System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
-        System.out.println(numbers[3]);
-        System.out.println(numbers[4]);
-        System.out.println();
-        System.out.println("Give two indices to swap: ");
-        int swap1 = scanner.nextInt();
-        int swap2 = scanner.nextInt();
-        int helper = numbers[swap1];
-        numbers[swap1] = numbers[swap2];
-        numbers[swap2] = helper;
-        System.out.println();
-        System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
-        System.out.println(numbers[3]);
-        System.out.println(numbers[4]);
+        numbers[4] = 7;
+        numbers[5] = 7;
+        numbers[6] = 7;
+
+        System.out.print("Search for? ");
+        int number = scanner.nextInt();
+        int index = 0;
+        while (index < numbers.length) {
+            if(number == numbers[index]){
+                System.out.println(number + " is at index "+ index + ".");
+            }
+            index++;
+        }
     }
 }
