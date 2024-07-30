51957f672acabe19e717f06e854907bb90b3a0e7
Yermek Tairkhan <trouzerop@gmail.com>
2024-07-30 16:05:08 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,26 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int smallest =0;
-        String oldestName = null;
+        int sum = 0;
+        int count = 0;
+        String smallest = "";
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("")) {
                 break;
             }
             String[] parts = input.split(",");
-            int oldest = Integer.parseInt(parts[1]);
-            oldestName= parts[0];
-            if(smallest < oldest){
-                smallest = oldest;
-                oldestName= parts[0];
+            int age = Integer.parseInt(parts[1]);
+            sum = sum+ age;
+            count++;
+            String longest = parts[0];
+            if(longest.length() > smallest.length()){
+              smallest = longest;
             }
 
         }
-        System.out.println("Age of the oldest: " + oldestName);
+        System.out.println("Longest name: " + smallest);
+        System.out.println("Average of the birth years: " + (1.0* sum/count));
 
 
     }
