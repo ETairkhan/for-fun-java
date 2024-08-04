d65201f586e3659dd0e20a997af7757b09aa2b99
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-04 16:19:16 +0600

    fun

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        Product bana = new Product("Banana", 1.1, 13);
-        bana.printProduct();
+        Person ada = new Person("Ada");
+        Person antti = new Person("Antti");
+
+        ada.printPerson();
+        antti.printPerson();
+        System.out.println("");
+
+        ada.growOlder();
+        ada.growOlder();
+
+        ada.printPerson();
+        antti.printPerson();
     }
 }
