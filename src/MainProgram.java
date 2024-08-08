4b89a5661058e4724516bed23c53566b904ff887
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-08 12:21:19 +0600

    fun

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -4,34 +4,7 @@ import java.util.ArrayList;
 public class MainProgram{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<book> books = new ArrayList<>();
-        while(true){
-            System.out.print("Name: ");
-            String name = scanner.nextLine();
-            if ( name.isEmpty()){
-                break;
-            }
-            System.out.print("Pages: ");
-            int pages = scanner.nextInt();
-
-            System.out.print("Publication year: ");
-            int year = scanner.nextInt();
-            scanner.nextLine();
-
-            books.add(new book(name,pages,year));
-
-        }
-        System.out.println("What information will be printed? ");
-        String what = scanner.nextLine();
-        if ( what.equals("everything")){
-            for(book i: books){
-                System.out.println(i);
-            }
-        }else if (what.equals("name")){
-            for(book i : books){
-                System.out.println(i.getNames());
-            }
-        }
+        //FIles and the Filesystem!!!!!
 
 
     }
