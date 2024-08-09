ce4c91044b8877ee6f9e9b437949752e733088d7
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-09 14:52:46 +0600

    fun

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -5,29 +5,36 @@ import java.util.ArrayList;
 public class MainProgram{
     public static void main(String[] args){
 
-        System.out.println("Name of the file");
+        System.out.print("File?");
         Scanner smt = new Scanner(System.in);
         String name = smt.nextLine();
         // we create a scanner for reading the file
         try (Scanner scanner = new Scanner(Paths.get(name))) {
-            ArrayList<String> list =new ArrayList<>();
+            ArrayList<Integer> list =new ArrayList<>();
             // we read the file until all lines have been read
             while (scanner.hasNextLine()) {
                 // we read one line
                 String row = scanner.nextLine();
-                list.add(row);
+                if (row.isEmpty()) {
+                    continue;
+                }
+                int numbers = Integer.parseInt(row);
+                list.add(numbers);
+
             }
             while(true){
-                System.out.println("Searcher for:");
-                String guest = smt.nextLine();
-                if(guest.isEmpty()){
-                    break;
-                }else if(list.contains(guest)){
-                    System.out.println("Found!");
-                }else {
-                    System.out.println("Not found.");
+                System.out.print("Lower bound?");
+                int lower = smt.nextInt();
+                System.out.print("Upper bound?");
+                int upper = smt.nextInt();
+                int count =0;
+                for(int i = 0; i < list.size(); i++){
+                    if(list.get(i) >= lower && list.get(i) <= upper){
+                        count++;
+                    }
                 }
-
+                System.out.println("Numbers: " + count );
+              break;
             }
         } catch (Exception e) {
             System.out.println("Reading the file " + name +" failed");
@@ -35,7 +42,7 @@ public class MainProgram{
             System.out.println("Attempted to access file: " + Paths.get("data.txt").toAbsolutePath());
 
         }
-        System.out.println("Thank you!");
+
 
 
 
