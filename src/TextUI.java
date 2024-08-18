c00b2decdf8020c69774553f7a76cf2518c5d6fe
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-18 17:05:03 +0600

    So basically it is silly mistake , I made a new instance variables for the TextUI constructor which is different from the variables in Main.ja

--- src/TextUI.java
+++ src/TextUI.java
@@ -5,18 +5,22 @@ public class TextUI {
     private SimpleDictionary simpleDictionary;
 
     public TextUI(Scanner scanner, SimpleDictionary simpleDictionary){
-        this.scanner = new Scanner(System.in);
-        this.simpleDictionary= new SimpleDictionary();
+        this.scanner =scanner ;
+        this.simpleDictionary= simpleDictionary;
 
     }
     public void start(){
         while(true){
             System.out.print("Command: ");
             String command = scanner.nextLine();
+
             if(command.equals("end")){
+
                 System.out.println("Bye bye!");
                 break;
+
             } else if(command.equals("add")){
+
                 System.out.print("Word: ");
                 String  word = scanner.nextLine();
 
@@ -26,15 +30,22 @@ public class TextUI {
                 simpleDictionary.add(word, translation);
 
             } else if (command.equals("search")) {
+
                 System.out.println("To be translated:");
                 String translateThis = scanner.nextLine();
+
                 if (simpleDictionary.translate(translateThis) == null) {
+
                     System.out.println("Word " + translateThis +" was not found");
+
                 } else {
+
                     System.out.println("Translation: " + simpleDictionary.translate(translateThis));
                 }
             } else {
+
                 System.out.println("Unknown command");
+
             }
         }
     }
