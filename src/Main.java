8211dc08f382ad02bc29fb0308cebdd02b338447
Yermek Tairkhan <trouzerop@gmail.com>
2024-08-21 12:18:08 +0600

    Make todo-list

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,39 @@ public class Main{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TodoList todoList = new TodoList();
-        Interface something = new Interface(todoList, scanner);
-        something.start();
+
+        GradeRegister register = new GradeRegister();
+
+        while (true) {
+            System.out.print("Points: ");
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            int score = Integer.valueOf(input);
+
+            if (score < 0 || score > 100) {
+                System.out.println("Impossible number.");
+                continue;
+            }
+
+            register.addGradeBasedOnPoints(score);
+        }
+
+        System.out.println("");
+        int grade = 5;
+        while (grade >= 0) {
+            int stars = register.numberOfGrades(grade);
+            System.out.print(grade + ": ");
+            while (stars > 0) {
+                System.out.print("*");
+                stars--;
+            }
+            System.out.println("");
+
+            grade = grade - 1;
+        }
+        //Separating the program logic is a major benefit for the maintenance of the program. Since the program logic — in this case the GradeRegister — is its own class, it can also be tested separately from the other parts of the program. If you wanted to, you could copy the class GradeRegister and use it in your other programs. Below is an example of simple manual testing — this experiment only concerns itself with a small part of the register's functionality.
     }
 }
